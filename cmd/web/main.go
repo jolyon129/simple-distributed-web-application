a3dfa617b04c684fb1252d535751f20635e62fdf
Zhuolun Li <zl2501@nyu.edu>
2020-03-18 22:19:51 -0400

    Complete Structure. Have a login page. Import code of session. TODO: understand the code of session.

---
+++ cmd/web/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "zl2501-final-project/web"
+
+func main() {
+	// Change working directory to web/ first
+	web.StartService()
+}
--- web/web.go
+++ web/web.go
@@ -2,11 +2,24 @@ package web
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strings"
 )
 
-func SayhelloName(w http.ResponseWriter, r *http.Request) {
+func StartService() {
+	http.HandleFunc("/", sayHelloName)       // set router
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	} else {
+		fmt.Println("Server starts at: localhost:9090")
+	}
+}
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       // parse arguments, you have to call this by yourself
 	fmt.Println(r.Form) // print form information in server side
 	fmt.Println("path", r.URL.Path)
@@ -17,4 +30,18 @@ func SayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("val:", strings.Join(v, ""))
 	}
 	fmt.Fprintf(w, "Hello Zhuolun!")
+}
+
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("template/login.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
