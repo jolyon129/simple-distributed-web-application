66f5687aa98cea4fadc5de92d4b9cc3b470fad21
Zhuolun Li <zl2501@nyu.edu>
2020-04-11 17:57:55 -0400

    Fix the bug of blocked buffer in repo
    Because I use select clause in repo to wait on multiple channels and
    try to access the value in one of the channels, I cannot use unbuffered
    channel. If context.Done() happen first, the unbuffered channel never
    get the chance to execute <- receive and this will block the sender of
    this channel which is the storage operation in DB!
    
    And then this will block all following same  repo operations!
    This take me hours to find out this bug!

--- backend/model/repository/tweetrepo.go
+++ backend/model/repository/tweetrepo.go
@@ -1,61 +1,61 @@
 package repository
 
 import (
-	"context"
-	"sync"
-	"time"
-	"zl2501-final-project/backend/model/storage"
+    "context"
+    "sync"
+    "time"
+    "zl2501-final-project/backend/model/storage"
 )
 
 type TweetRepo struct {
-	sync.Mutex
-	storage storage.TweetStorageInterface
+    sync.Mutex
+    storage storage.TweetStorageInterface
 }
 
 type TweetInfo struct {
-	UserID  uint
-	Content string
+    UserID  uint
+    Content string
 }
 
 func (postRepo *TweetRepo) SaveTweet(ctx context.Context, p TweetInfo) (uint, error) {
-	result := make(chan uint)
-	errorChan := make(chan error)
-	go func() {
-		postRepo.storage.Create(&storage.TweetEntity{
-			ID:          0,
-			UserID:      p.UserID,
-			Content:     p.Content,
-			CreatedTime: time.Time{},
-		}, result, errorChan)
-	}()
-	select {
-	case ret := <-result:
-		return ret, nil
-	case err := <-errorChan:
-		return 0, err
-	case <-ctx.Done():
-		return 0, ctx.Err()
-	}
+    result := make(chan uint, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        postRepo.storage.Create(&storage.TweetEntity{
+            ID:          0,
+            UserID:      p.UserID,
+            Content:     p.Content,
+            CreatedTime: time.Time{},
+        }, result, errorChan)
+    }()
+    select {
+    case ret := <-result:
+        return ret, nil
+    case err := <-errorChan:
+        return 0, err
+    case <-ctx.Done():
+        return 0, ctx.Err()
+    }
 }
 
 // Return a post Entity according the post id
 func (postRepo *TweetRepo) SelectById(ctx context.Context, pId uint) (*storage.TweetEntity, error) {
-	result := make(chan *storage.TweetEntity)
-	errorChan := make(chan error)
-	go func() {
-		postRepo.storage.Read(pId, result, errorChan)
-	}()
-	select {
-	case tweet := <-result:
-		return tweet, nil
-	case err := <-errorChan:
-		return nil, err
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
+    result := make(chan *storage.TweetEntity, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        postRepo.storage.Read(pId, result, errorChan)
+    }()
+    select {
+    case tweet := <-result:
+        return tweet, nil
+    case err := <-errorChan:
+        return nil, err
+    case <-ctx.Done():
+        return nil, ctx.Err()
+    }
 }
 
 func NewTweetRepo(storageInterface storage.TweetStorageInterface) *TweetRepo {
-	ret := TweetRepo{storage: storageInterface}
-	return &ret
+    ret := TweetRepo{storage: storageInterface}
+    return &ret
 }
--- backend/model/repository/userrepo.go
+++ backend/model/repository/userrepo.go
@@ -1,177 +1,177 @@
 package repository
 
 import (
-	"context"
-	"errors"
-	"sync"
-	"zl2501-final-project/backend/model/storage"
+    "context"
+    "errors"
+    "sync"
+    "zl2501-final-project/backend/model/storage"
 )
 
 // This is a singleton type
 type UserRepo struct {
-	//	sync.Mutex // Need lock
-	storage storage.UserStorageInterface
-	con     *sync.Cond
+    //	sync.Mutex // Need lock
+    storage storage.UserStorageInterface
+    con     *sync.Cond
 }
 
 type UserInfo struct {
-	UserName string
-	Password string
+    UserName string
+    Password string
 }
 
 func NewUserRepo(storageInterface storage.UserStorageInterface) *UserRepo {
-	ret := UserRepo{
-		storage: nil,
-	}
-	ret.storage = storageInterface
-	return &ret
+    ret := UserRepo{
+        storage: nil,
+    }
+    ret.storage = storageInterface
+    return &ret
 }
 
 // Create a new user and return user id
 // if the user name is not duplicated.
 // Otherwise return error
 func (userRepo *UserRepo) CreateNewUser(ctx context.Context, u *UserInfo) (uint, error) {
-	result := make(chan uint)
-	errorChan := make(chan error)
-	go func() {
-		userRepo.storage.Create(&storage.UserEntity{
-			ID:       0,
-			UserName: u.UserName,
-			Password: u.Password,
-		}, result, errorChan)
-	}()
-
-	select {
-	case ret := <-result:
-		return ret, nil
-	case err := <-errorChan:
-		return 0, err
-	case <-ctx.Done():
-		return 0, ctx.Err()
-	}
+    result := make(chan uint, 1) // Have to use buffered channel bc th select
+    errorChan := make(chan error, 1)
+    go func() {
+        userRepo.storage.Create(&storage.UserEntity{
+            ID:       0,
+            UserName: u.UserName,
+            Password: u.Password,
+        }, result, errorChan)
+    }()
+    select {
+    case ret := <-result:
+        return ret, nil
+    case err := <-errorChan:
+        return 0, err
+    case <-ctx.Done():
+        return 0, ctx.Err()
+    }
 }
 
-func (userRepo *UserRepo) SelectByName(ctx context.Context, name string) (*storage.UserEntity, error) {
-	result := make(chan []*storage.UserEntity)
-	errorChan := make(chan error)
-	go func() {
-		userRepo.storage.FindAll(result, errorChan)
-	}()
-	select {
-	case users := <-result:
-		for _, value := range users {
-			if value.UserName == name {
-				return value, nil
-			}
-		}
-		return nil, errors.New("the name does not exist")
-	case err := <-errorChan:
-		return nil, err
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
+func (userRepo *UserRepo) SelectByName(ctx context.Context, name string) (*storage.UserEntity,
+        error) {
+    result := make(chan []*storage.UserEntity, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        userRepo.storage.FindAll(result, errorChan)
+    }()
+    select {
+    case users := <-result:
+        for _, value := range users {
+            if value.UserName == name {
+                return value, nil
+            }
+        }
+        return nil, errors.New("the name does not exist")
+    case err := <-errorChan:
+        return nil, err
+    case <-ctx.Done():
+        return nil, ctx.Err()
+    }
 }
 
 func (userRepo *UserRepo) SelectById(ctx context.Context, uid uint) (*storage.UserEntity, error) {
-	result := make(chan *storage.UserEntity)
-	errorChan := make(chan error)
-	go func() {
-		userRepo.storage.Read(uid, result, errorChan)
-	}()
-	select {
-	case ret := <-result:
-		return ret, nil
-	case err := <-errorChan:
-		return nil, err
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
+    result := make(chan *storage.UserEntity, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        userRepo.storage.Read(uid, result, errorChan)
+    }()
+    select {
+    case ret := <-result:
+        return ret, nil
+    case err := <-errorChan:
+        return nil, err
+    case <-ctx.Done():
+        return nil, ctx.Err()
+    }
 }
 
 // Add the tweet id into the user
 func (u *UserRepo) AddTweetToUser(ctx context.Context, uId uint, pId uint) (bool, error) {
-	result := make(chan bool)
-	errorChan := make(chan error)
-	go func() {
-		u.storage.AddTweetToUserDB(uId, pId, result, errorChan)
-	}()
-	select {
-	case <-result:
-		return true, nil
-	case err := <-errorChan:
-		return false, err
-	case <-ctx.Done():
-		return false, ctx.Err()
-	}
+    result := make(chan bool, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        u.storage.AddTweetToUserDB(uId, pId, result, errorChan)
+    }()
+    select {
+    case <-result:
+        return true, nil
+    case err := <-errorChan:
+        return false, err
+    case <-ctx.Done():
+        return false, ctx.Err()
+    }
 }
 
 // Return all users in the database
 func (u *UserRepo) FindAllUsers(ctx context.Context) ([]*storage.UserEntity, error) {
-	result := make(chan []*storage.UserEntity)
-	errorChan := make(chan error)
-	go func() {
-		u.storage.FindAll(result, errorChan)
-	}()
-	select {
-	case users := <-result:
-		return users, nil
-	case err := <-errorChan:
-		return nil, err
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
+    result := make(chan []*storage.UserEntity, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        u.storage.FindAll(result, errorChan)
+    }()
+    select {
+    case users := <-result:
+        return users, nil
+    case err := <-errorChan:
+        return nil, err
+    case <-ctx.Done():
+        return nil, ctx.Err()
+    }
 }
 
 // Check whether the user srcId follows the user targetId.
 // Take O(#following) time
 func (u *UserRepo) CheckWhetherFollowing(ctx context.Context, srcId uint, targetId uint) (bool,
-	error) {
-	result := make(chan bool)
-	errorChan := make(chan error)
-	go func() {
-		u.storage.CheckWhetherFollowingDB(srcId, targetId, result, errorChan)
-	}()
-	select {
-	case ret := <-result:
-		return ret, nil
-	case err := <-errorChan:
-		return false, err
-	case <-ctx.Done():
-		return false, ctx.Err()
-	}
+        error) {
+    result := make(chan bool, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        u.storage.CheckWhetherFollowingDB(srcId, targetId, result, errorChan)
+    }()
+    select {
+    case ret := <-result:
+        return ret, nil
+    case err := <-errorChan:
+        return false, err
+    case <-ctx.Done():
+        return false, ctx.Err()
+    }
 }
 
 // User srcId starts to follow targetId.
 func (u *UserRepo) StartFollowing(ctx context.Context, srcId uint, targetId uint) (bool, error) {
-	result := make(chan bool)
-	errorChan := make(chan error)
-	go func() {
-		u.storage.StartFollowingDB(srcId, targetId, result, errorChan)
-	}()
-	select {
-	case err := <-errorChan:
-		return false, err
-	case ret := <-result:
-		return ret, nil
-	case <-ctx.Done():
-		return false, ctx.Err()
-	}
+    result := make(chan bool, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        u.storage.StartFollowingDB(srcId, targetId, result, errorChan)
+    }()
+    select {
+    case err := <-errorChan:
+        return false, err
+    case ret := <-result:
+        return ret, nil
+    case <-ctx.Done():
+        return false, ctx.Err()
+    }
 }
 
 // srcId stop following targetId.
 // targetId remove the follower srcId.
 func (u *UserRepo) StopFollowing(ctx context.Context, srcId uint, targetId uint) (bool, error) {
-	result := make(chan bool)
-	errorChan := make(chan error)
-	go func() {
-		u.storage.StopFollowingDB(srcId, targetId, result, errorChan)
-	}()
-	select {
-	case ret := <-result:
-		return ret, nil
-	case err := <-errorChan:
-		return false, err
-	case <-ctx.Done():
-		return false, nil
-	}
+    result := make(chan bool, 1)
+    errorChan := make(chan error, 1)
+    go func() {
+        u.storage.StopFollowingDB(srcId, targetId, result, errorChan)
+    }()
+    select {
+    case ret := <-result:
+        return ret, nil
+    case err := <-errorChan:
+        return false, err
+    case <-ctx.Done():
+        return false, ctx.Err()
+    }
 }
