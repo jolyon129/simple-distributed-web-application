03b86577903e04998461f392f0c7637fcd385327
Zhuolun Li <zl2501@nyu.edu>
2020-04-13 17:53:00 -0400

    combine middleware into a single file

--- web/middleware/middleware.go
+++ web/middleware/middleware.go
@@ -5,9 +5,34 @@ import (
     "log"
     "net/http"
     "net/url"
+    "os"
+    "time"
     "zl2501-final-project/web/constant"
     "zl2501-final-project/web/pb"
 )
+
+func init() {
+}
+
+// logRequestsMiddleware is a middleware handler which implement the handler interface
+type logRequestsMiddleware struct {
+    handler http.Handler
+    logger  *log.Logger
+}
+
+func (l *logRequestsMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    l.handler.ServeHTTP(w, r)
+    l.logger.Printf("Request:%s %s, Time: %v", r.Method, r.URL.Path, time.Since(start))
+}
+
+func LogRequests(handlerToWrap http.Handler) *logRequestsMiddleware {
+    logger := log.New(os.Stdout, "LogRequests:", log.Ltime|log.Lshortfile)
+    return &logRequestsMiddleware{
+        handler: handlerToWrap,
+        logger:  logger,
+    }
+}
 
 // This is a middleware handler used to check weather this request is authenticated.
 // If not, redirect to the index.
