500e660f0110072b574932bc3e22e10866c71601
Zhuolun Li <zl2501@nyu.edu>
2020-04-29 19:30:44 -0400

    delete redundent code

--- web/web.go
+++ web/web.go
@@ -1,10 +1,8 @@
 package web
 
 import (
-    "context"
     "log"
     "net/http"
-    "time"
     "zl2501-final-project/web/constant"
     "zl2501-final-project/web/controller"
     . "zl2501-final-project/web/middleware"
@@ -17,18 +15,6 @@ func init() {
     log.SetPrefix("GlobalLogger: ")
     log.SetFlags(log.Ltime | log.Lshortfile)
     // Set up a connection to the server.
-
-}
-
-func GreetHello() {
-    // Contact the server and print out its response.
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    r, err := pb.BackendClientIns.SayHello(ctx, &pb.HelloRequest{Name: "Zhuolun"})
-    if err != nil {
-        log.Fatalf("could not greet: %v", err)
-    }
-    log.Printf("Greeting: %s", r.GetMessage())
 
 }
 
