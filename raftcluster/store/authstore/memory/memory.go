aa98bc48d058e78a7fd2d9d3799e520b9b23f949
Zhuolun Li <zl2501@nyu.edu>
2020-05-02 15:02:16 -0400

    Fix concurrency bug when marshal
    The smaller lock in MemSess should not depend on the bigger lock in
    Provider

--- raftcluster/store/authstore/memory/memory.go
+++ raftcluster/store/authstore/memory/memory.go
@@ -11,6 +11,11 @@ import (
 
 var pder = &Provider{list: list.New()}
 
+func init() {
+    pder.sessions = make(map[string]*list.Element, 0)
+    storage.RegisterProvider("memory", pder)
+}
+
 // MemSessStore implement the session interface
 type MemSessStore struct {
     storage.SessionStorageInterface
@@ -21,17 +26,21 @@ type MemSessStore struct {
 }
 
 func (st *MemSessStore) Set(key, value interface{}) error {
+    pder.lock.Lock()
+    defer pder.lock.Unlock()
     st.Lock()
     defer st.Unlock()
     st.value[key] = value
-    pder.SessionUpdate(st.sid)
+    pder.SessionUpdateNonSync(st.sid)
     return nil
 }
 
 func (st *MemSessStore) Get(key interface{}) interface{} {
+    pder.lock.Lock()
+    defer pder.lock.Unlock()
     st.Lock()
     defer st.Unlock()
-    pder.SessionUpdate(st.sid)
+    pder.SessionUpdateNonSync(st.sid)
     if v, ok := st.value[key]; ok {
         return v
     } else {
@@ -40,10 +49,12 @@ func (st *MemSessStore) Get(key interface{}) interface{} {
 }
 
 func (st *MemSessStore) Delete(key interface{}) error {
+    pder.lock.Lock()
+    defer pder.lock.Unlock()
     st.Lock()
     defer st.Unlock()
     delete(st.value, key)
-    pder.SessionUpdate(st.sid)
+    pder.SessionUpdateNonSync(st.sid)
     return nil
 }
 
@@ -76,7 +87,7 @@ func (pder *Provider) SessionRead(sid string) (storage.SessionStorageInterface, 
     pder.lock.Lock()
     defer pder.lock.Unlock()
     if element, ok := pder.sessions[sid]; ok {
-        //pder.SessionUpdate(sid)
+        //pder.SessionUpdateNonSync(sid)
         return element.Value.(*MemSessStore), nil
     } else {
         return nil, fmt.Errorf("the session Id: %s is not existed", sid)
@@ -113,9 +124,18 @@ func (pder *Provider) SessionGC(maxlifetime int64) {
     }
 }
 
-func (pder *Provider) SessionUpdate(sid string) error {
+func (pder *Provider) SessionUpdateSync(sid string) error {
     pder.lock.Lock()
     defer pder.lock.Unlock()
+    if element, ok := pder.sessions[sid]; ok {
+        element.Value.(*MemSessStore).timeAccessed = time.Now()
+        pder.list.MoveToFront(element)
+        return nil
+    }
+    return nil
+}
+
+func (pder *Provider) SessionUpdateNonSync(sid string) error {
     if element, ok := pder.sessions[sid]; ok {
         element.Value.(*MemSessStore).timeAccessed = time.Now()
         pder.list.MoveToFront(element)
@@ -127,20 +147,21 @@ func (pder *Provider) SessionUpdate(sid string) error {
 // Get a snapshot of the data structure under the hood
 // Return a json of byte array, like marshall
 func (pder *Provider) GetSnapshot() ([]byte, error) {
+    return pder.MarshalJSON()
+}
+
+func (pder *Provider) MarshalJSON() ([]byte, error) {
+    println("Going to lock provider")
     pder.lock.Lock()
     defer pder.lock.Unlock()
     // to encode a Go map type it must be of the form map[string]T
     // (where T is any Go type supported by the json package).
     // https://blog.golang.org/json
-    sessArr := make([]map[string]interface{}, pder.list.Len())
+    sessArr := make([]json.RawMessage, pder.list.Len())
     i := 0
     for e := pder.list.Front(); e != nil; e = e.Next() {
         sess := e.Value.(*MemSessStore)
-        m := make(map[string]interface{})
-        for k, v := range sess.value { // convert map[interface]interface to map[string]interface
-            m[k.(string)] = v
-        }
-        m["timeAccessed"] = sess.timeAccessed
+        m, _ := sess.MarshalJSON()
         sessArr[i] = m
         i++
     }
@@ -151,6 +172,9 @@ func (pder *Provider) GetSnapshot() ([]byte, error) {
 }
 
 func (m *MemSessStore) MarshalJSON() ([]byte, error) {
+    println("Going to lock memSess")
+    m.Lock()
+    defer m.Unlock()
     value := make(map[string]interface{})
     for k, v := range m.value { // convert map[interface]interface to map[string]interface
         value[k.(string)] = v
@@ -160,9 +184,4 @@ func (m *MemSessStore) MarshalJSON() ([]byte, error) {
         "value":        value,
         "sid":          m.sid,
     })
-}
-
-func init() {
-    pder.sessions = make(map[string]*list.Element, 0)
-    storage.RegisterProvider("memory", pder)
 }
--- raftcluster/store/authstore/memory/memory_test.go
+++ raftcluster/store/authstore/memory/memory_test.go
@@ -3,6 +3,7 @@ package memory_test
 import (
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
+    "sync"
     authstorage "zl2501-final-project/raftcluster/store/authstore"
     _ "zl2501-final-project/raftcluster/store/authstore/memory"
 )
@@ -11,12 +12,14 @@ var _ = Describe("Memory", func() {
     var provider authstorage.ProviderInterface
     fakeSessId := "1j28v6loBj65ypDacf5VJxRDXDcDRU8y1RkdXNOu4qo%3D"
     fakeSessId2 := "1j28v6loBj65ypDacf5VJxRDXDcDRU8y1RkdXNOu4qo%2F"
+    //fakeSessId3 := "1j28v6loBj65ypDacf5VJxRDXDcDRU8y1RkdXNOu4qo%2F"
+    //fakeSessId4 := "1j28v6loBj65ypDacf5VJxRDXDcDRU8y1RkdXNOu4qo%2F"
     BeforeSuite(func() {
         p, _ := authstorage.GetProvider("memory")
         provider = p
     })
     Describe("Marshal session provider", func() {
-        Context("When have a list of sessions", func() {
+        PContext("When have a list of sessions", func() {
             It("should return a JSON list with map object inside", func() {
                 provider.SessionInit(fakeSessId)
                 provider.SessionInit(fakeSessId2)
@@ -32,6 +35,32 @@ var _ = Describe("Memory", func() {
                 //println(js)
                 Expect(js).Should(ContainSubstring(`"ins":"jolyon_z"`), ContainSubstring(`"id":3334`))
                 Expect(err).Should(BeNil())
+            })
+        })
+        Context("When marshal concurrently", func() {
+            It("Should be synced", func() {
+                var wg sync.WaitGroup
+                wg.Add(4)
+                for i := 0; i < 4; i++ {
+                    go func(i int) {
+                        provider.SessionInit(fakeSessId)
+                        provider.SessionInit(fakeSessId2)
+                        sess, _ := provider.SessionRead(fakeSessId)
+                        sess.Set("Name", "Zhuolun")
+                        sess.Set("Twitter", "jolyon129")
+                        sess.Set("id", 3334)
+                        sess1, _ := provider.SessionRead(fakeSessId2)
+                        sess1.Set("ins", "jolyon_z")
+                        sess1.Set("gender", "male")
+                        j, err := provider.GetSnapshot()
+                        js := string(j)
+                        println(js)
+                        Expect(js).Should(ContainSubstring(`"ins":"jolyon_z"`), ContainSubstring(`"id":3334`))
+                        Expect(err).Should(BeNil())
+                        wg.Done()
+                    }(i)
+                }
+                wg.Wait()
             })
         })
     })
